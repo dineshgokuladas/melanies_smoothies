e965a98c40d0418132ae531eca7db7f29a26c49a
dineshgokuladas <dinesh.gokuladas@digitalsoftwareinc.in>
2024-12-27 15:58:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -43,9 +44,9 @@ if ingredients_list:
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
             values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
 
-    import requests
+    
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-    st.text(smoothiefroot_response.json)
+    st.text(smoothiefroot_response.json())
     
     time_to_insert = st.button('Submit Order')
     
